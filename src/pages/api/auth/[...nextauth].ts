36f96af62aa7196c7f8d8d5b8a2e6a695a3418b5
Trisonweru <isaactrisonwaweru@gmail.com>
2022-01-31 21:27:08 +0300

    removed some folders

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-//import { PrismaClient } from '@prisma/client';
+
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 import prisma from '../../../lib/prisma';
-//const prisma = new PrismaClient();
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
