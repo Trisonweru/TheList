3709d4469bdfa8d44624038536d6d1b79bd06d8b
Trisonweru <isaactrisonwaweru@gmail.com>
2022-02-01 08:15:01 +0300

    added progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import ProgressBar from '@badrap/bar-of-progress';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SessionProvider } from 'next-auth/react';
@@ -9,11 +10,24 @@ import { darkTheme } from '../styles/theme/lightTheme';
 import createEmotionCache from '../../utils/createEmotionCache';
 
 const clientSideEmotionCache = createEmotionCache();
+import { Router } from 'next/router';
+
 import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
 
 import Layout from '@/components/layout/Layout';
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#fff',
+  className: 'bar-of-progress',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 const MyApp = (props: {
   Component: any;
