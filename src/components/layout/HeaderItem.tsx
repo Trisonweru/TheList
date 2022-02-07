96908a06e60f226d24c82e1c8f09f95b8e0e3588
Trisonweru <isaactrisonwaweru@gmail.com>
2022-02-07 05:23:24 +0300

    major changes

--- src/components/layout/HeaderItem.tsx
+++ src/components/layout/HeaderItem.tsx
@@ -10,7 +10,7 @@ const HeaderItem = ({ title, Icon, url }: props) => {
   return (
     <div
       onClick={url ? () => router.push(url) : null}
-      className='group flex w-12 cursor-pointer flex-col items-center hover:text-white sm:w-20'
+      className='group flex w-14 cursor-pointer flex-col items-center space-y-4 hover:text-white sm:w-20'
     >
       <Icon className='mb-1 h-6 group-hover:animate-bounce' />
       <p className='whitespace-nowrap tracking-widest opacity-0 group-hover:opacity-100 '>
